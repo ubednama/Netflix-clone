0dd99513946581c7906926307d2eb6e08606b824
ubednama <shahidkhan.ak18@gmail.com>
2024-03-30 04:33:49 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -40,12 +40,12 @@ app.use(cors(corsOptions));
 //   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 // });
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
-        //file is saved in 'dist' for better optimization
+app.use(express.static(path.join(__dirname, "/frontend/build")))
+        //file is saved in 'build' for better optimization
 
 
 app.get("*",(req, res) => {
-    res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname,"frontend", "build", "index.html"));
 })
 
 
