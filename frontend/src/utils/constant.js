80fd697a77814db2ceac7f24f9a3f6f934bd9e63
ubednama <shahidkhan.ak18@gmail.com>
2024-03-30 04:46:17 +0530

    first commit

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
 // export const API_END_POINT = "http://localhost:8080/api/v1/user"
-export const API_END_POINT = 'https://netflix-clone-gezd.onrender.com'
+export const API_END_POINT = 'https://netflix-clone-gezd.onrender.com/api/v1/user'
 
 export const options = {
     method: 'GET',
